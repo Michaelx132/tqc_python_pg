31be7e44350499f926bd1052fbf1b4ecc23ece45
陳聖葦 <sw132@pic.net.tw>
2023-04-18 11:22:55 +0800

    20230418

--- P701.py
+++ P701.py
@@ -3,6 +3,8 @@ import io
 # 設計說明：
 # (1) 請撰寫一個程式，讓使用者輸入一個 10 字元以內的二進位字串，
 # 將其轉換成十進位並輸出
+# 技巧
+# 次方 n ** 2  = n 的2次方
 
 inputStr = list(input().strip())
 score = 0
@@ -12,15 +14,12 @@ try:
     n=0
     for x in range(len(inputStr)-1,-1,-1):
         if (x==0):
-            print("A")
-#            if (inputStr[n]=='1'):
-#                score = score + 1
+            if (inputStr[n] == '1'):
+                score = score + 2 ** x
         else:
-            print("B")
-            if (inputStr[n]=="1"):
-                print("C")
-#                score = score + inputStr[n]^x
+            if (inputStr[n] == '1'):
+                score = score + 2 ** x
         n = n+1
-#    print(score)
+    print(score)
 except:
     print("error")
